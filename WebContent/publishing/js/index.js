5280d36e68876da9e4d42002bc8917efb7be02e7
lee-tae-in <ti7209737@gmail.com>
2023-10-14 18:43:14 +0900

    리뷰 등록창(별점만)

--- WebContent/publishing/js/index.js
+++ WebContent/publishing/js/index.js
@@ -46,9 +46,67 @@ $(function(){
     $(".cancel").on("click",function(e){
         $(this).parents(".popup-wrap").hide();
     });
+
+    $(".drhg").on("click",function(e){
+        $(this).parents(".uppopup").show();
+    });
+
+    $(".upno").click(function(){
+        $(".uppopup").hide();
+    });
+
+    $(".drhg").click(function(){
+        $(".uppopup").show();
+    });
+
+    $(".flqb").click(function(){
+        $(".info-popup2").show();
+    });
+
+    $(".cnrk").click(function(){
+        $(".modal").hide();
+    });
+
+    // 별점
+    // JavaScript 변수를 초기화합니다.
+    let userRating = 0;
+
+    // 별점을 클릭할 때 호출될 함수
+    function handleStarClick(event) {
+    const clickedStar = event.target;
+    const rating = parseInt(clickedStar.getAttribute("data-rating"));
+
+    // 사용자가 클릭한 별점을 변수에 저장합니다.
+    userRating = rating;
+
+    // 모든 별점을 리셋합니다.
+    const stars = document.querySelectorAll(".star");
+    stars.forEach(star => {
+        star.textContent = "☆";
+    });
+
+    // 클릭한 별점까지만 채웁니다.
+    for (let i = 0; i < rating; i++) {
+        stars[i].textContent = "★";
+    }
+    }
+
+    // 별점을 클릭할 때 함수를 연결합니다.
+    const stars = document.querySelectorAll(".star");
+    stars.forEach(star => {
+    star.addEventListener("click", handleStarClick);
+    });
+        
+    $(".cnrk").click(function(){
+        console.log(userRating);
+    });
+
+
 });
 
 function exam() {
     // 예시 함수
     console.log("브라우저 시작");
 }
+
+
