c3259219630aaeed2256e72b0410c77a77d34960
wjdgns06 <greathunny2006@gmail.com>
2023-10-14 18:04:46 +0900

    json util camel case 적용

--- src/common/JsonUtil.java
+++ src/common/JsonUtil.java
@@ -3,6 +3,8 @@ package common;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class JsonUtil {
 	// 단일 JSON return
@@ -13,7 +15,7 @@ public class JsonUtil {
 		JSONObject obj = new JSONObject();
 
 		for (int i = 0; i < columns; i++)
-			obj.put(resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase(), resultSet.getObject(i + 1));
+			obj.put(makeCamelCase(resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase()), resultSet.getObject(i + 1));
 		
 		return obj;
 	}
@@ -28,10 +30,19 @@ public class JsonUtil {
 			JSONObject obj = new JSONObject();
 
 			for (int i = 0; i < columns; i++)
-				obj.put(resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase(), resultSet.getObject(i + 1));
+				obj.put(makeCamelCase(resultSet.getMetaData().getColumnLabel(i + 1).toLowerCase()), resultSet.getObject(i + 1));
 
 			jsonArray.put(obj);
 		}
 		return jsonArray;
 	}
+	
+	public static String makeCamelCase(final String str) {
+		String result = str.indexOf("_") != -1
+	        ? str.substring(0, str.indexOf("_")) + 
+	            Arrays.stream(str.substring(str.indexOf("_") + 1).split("_"))
+	            .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1)).collect(Collectors.joining())
+	        : str;
+	    return result;
+	  }
 }
